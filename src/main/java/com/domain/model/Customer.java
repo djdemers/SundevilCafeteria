3c88b5d6b9c9542a1bff70c5069c0d52c7599e1e
David Joseph De Mers <djdemers@asu.edu>
2024-10-30 10:11:30 -0700

    basics

--- src/main/java/com/domain/model/Customer.java
+++ src/main/java/com/domain/model/Customer.java
@@ -1,4 +1,27 @@
 package com.domain.model;
 
 public class Customer {
+    private String customerId;
+    private String name;
+
+    public Customer(String customerId, String name) {
+        this.customerId = customerId;
+        this.name = name;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
